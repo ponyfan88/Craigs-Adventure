d42ce99c620a501286821cdf5d6cf4077a5e2fda
XanderMooney <PS24XMooney@efcts.us>
2022-11-12 18:20:03 -0500

    Goblin now works (almost) fully

--- Assets/Scripts/AI/Enemy Specific/goblinStateManager.cs
+++ Assets/Scripts/AI/Enemy Specific/goblinStateManager.cs
@@ -3,30 +3,52 @@ public class goblinStateManager : MonoBehaviour
 {
     ProjectileSpawner projectileSpawner;
     GameObject player;
+    Animator animator;
     Vector2 distance;
+    LayerMask raycastMask;
     private void Awake()
     {
         projectileSpawner = GetComponent<ProjectileSpawner>();
+        animator = GetComponent<Animator>();
         player = GameObject.Find("player");
+
+        // set the raycast's layermask to only include "IgnoreCollision"
+        raycastMask = LayerMask.NameToLayer("IgnoreCollision");
+        // using a bitwise operator to invert the layermask, meaning that the layer "IgnoreCollision" should be completely ignored.
+        // raycastMask = ~raycastMask;
     }
     public void TrySpit()
     {
-        distance = player.transform.position - transform.position;
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, distance);
-        Debug.DrawRay(transform.position, distance, Color.red, 1f);
+         distance = player.transform.position - transform.position;
+         /* Physics2D.IgnoreLayerCollision(raycastMask, raycastMask);
 
-        if (ray.collider.name != "player" && ray.collider.transform != transform && ray.collider.gameObject.layer != LayerMask.NameToLayer("IgnoreCollision"))
-        {
-            // Logic to avoid walls
-        }
-        else
-        {
-            Spit();
-        }
+         RaycastHit2D ray = Physics2D.Raycast(transform.position, distance);
+         Debug.DrawRay(transform.position, distance, Color.red, 1f);
+
+         if (ray.collider != null)
+         {
+             // Logic to avoid walls
+             Debug.Log("Blocked shot by: " + ray.collider.name);
+         }
+         else
+         {
+             Spit();
+
+             if (ray.collider.name != null)
+                 Debug.Log(ray.collider.name);
+         } */
+
+
+        // The code above WILL BE USED; its just raycasting is causing me issues with ignoring specific layers, for now i'm just gon make goblin spit
+        Spit();
     }
     public void Spit()
     {
-        Debug.Log("Spit");
         projectileSpawner.SpawnBullets();
+
+        if (distance.x > 0.5 || distance.y > 0.5)
+        {
+            animator.SetBool("reachedLocation", false);
+        }
     }
 }
