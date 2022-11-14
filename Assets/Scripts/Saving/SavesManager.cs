756d907b738d2c6eadf76c3ccc7f56a4a1a7a220
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-11-14 09:12:54 -0500

    comments

--- Assets/Scripts/Saving/SavesManager.cs
+++ Assets/Scripts/Saving/SavesManager.cs
@@ -15,9 +15,9 @@ public class SavesManager : MonoBehaviour
     #region Variables
     
     public int savesCount = 0; // we'll set this later
-    public bool loadingSave = false;
-    public int seed = 1;
-    public Save currentSave = new Save();
+    public bool loadingSave = false; // we arent loading a save by default
+    public int seed = 1; // our seed (initialize as 1 since thats a good, noticeable seed. it generates a sort of upside-down stairs pattern.)
+    public Save currentSave = new Save(); // our save. see save.cs for this classes info. it's in the same directory.
     
     SqliteManager sqliteManager;
     Pause pause;
@@ -26,6 +26,11 @@ public class SavesManager : MonoBehaviour
 
     #region Default Methods
 
+    /*
+     * purpose: various setup things.
+     * inputs: clones of ourselves, existing save files.
+     * outputs: save count and saves folder, as well as variables needed for the custom methods to run
+     */
     private void Start()
     {
         // get every saving object
@@ -63,8 +68,16 @@ public class SavesManager : MonoBehaviour
 
     #region Custom Methods
 
+    /*
+     * purpose: tells the sqlite manager to save our game
+     * inputs: the save name
+     * outputs: see purpose
+     */
     public void saveScene(string saveName = null) // called by pause
     {
+        // here we check to make sure that our input string is totally ok to be a save name
+        // "\u200b" is the default (empty) input - but we check for other ones just in case.
+        // this isnt called every frame, so im not worried about any extra checks just in case
         if (string.IsNullOrEmpty(saveName) || string.IsNullOrWhiteSpace(saveName) || saveName == "\u200b")
         {
             saveName = "my save";
@@ -75,16 +88,19 @@ public class SavesManager : MonoBehaviour
         DirectoryInfo savesFolder = new DirectoryInfo(Application.dataPath + "//saves//"); // get the saves folder and scan it for .jsave files
         FileInfo[] saves = savesFolder.GetFiles(saveName + ".sqlite");
 
-        if (saves.Length > 0)
+        if (saves.Length > 0) // this means that we found a file with the same name
         {
             LogToFile.Log("save \"" + saveName + ".sqlite\" existed. deleting save.");
-            try
+            
+            try // try to delete the file. this only works if we havent saved the file this session
             {
                 saves[0].Delete();
                 LogToFile.Log("deleted file.");
             }
-            catch
+            catch // this throws if we've already saved a file by this name this session
             {
+                // to explain why this is okay, this means our sqlite manager will just open the existing save and write over the data.
+                // so why check in the first place? well, above actually is for OUTDATED saves, and as such we'd potentially be trying to find tables that dont exist - causing a crash
                 LogToFile.Log("we cannot delete the file, but thats okay since its the same save version.");
             }
         }
@@ -92,6 +108,11 @@ public class SavesManager : MonoBehaviour
         sqliteManager.Write(saveName);
     }
 
+    /*
+     * purpose: tells the sqlite manager to load our game
+     * inputs: the save name
+     * outputs: see purpose
+     */
     public void loadScene(string saveName = null) // called by pause
     {
         if (string.IsNullOrEmpty(saveName) || string.IsNullOrWhiteSpace(saveName) || saveName == "\u200b")
@@ -104,6 +125,7 @@ public class SavesManager : MonoBehaviour
 
         loadingSave = true; // we are now loading the save
 
+        // loading save is saved between restarts, so its okay to restart our game
         pause.RestartGame(); // restart our game
     }
 
