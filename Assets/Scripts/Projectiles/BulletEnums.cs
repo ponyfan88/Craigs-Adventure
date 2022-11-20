c7b5ab6bb5b1c2ca87e9999e7019c7753fe71c05
XanderMooney <PS24XMooney@efcts.us>
2022-11-20 01:00:51 -0500

    Removed unused Enum :skull:

--- Assets/Scripts/Projectiles/BulletEnums.cs
+++ Assets/Scripts/Projectiles/BulletEnums.cs
@@ -10,6 +10,4 @@ namespace BulletEffects
    public enum Bulletpaths {linear,sineWave,SpeedChange, }
    public enum endlifeEffect{none,shatter,}
    // potental effects when the bullet reaches the end of it's life
-   public enum collisonEffect { Destroy, bounce, returnToThrower, }
-   // i just added some that sound usful feel free to add more
 }
