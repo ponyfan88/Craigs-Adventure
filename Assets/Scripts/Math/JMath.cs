1e630d2e7e6aaa3e16c984f610460d674bbb758c
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-11-22 10:29:48 -0500

    even more additional methods

--- Assets/Scripts/Math/JMath.cs
+++ Assets/Scripts/Math/JMath.cs
@@ -5,12 +5,18 @@
  * Outputs: math functions
  */
 
+using System;
+using UnityEngine;
+
 public static class JMath
 {
     #region Variables
 
     public const int MAXINT = 2147483647;
     public const long MAXLONG = 9223372036854775807;
+
+    public const byte MAXINTLENGTH = 10;
+    public const byte MAXLONGLENGTH = 19;
 
     #endregion
 
@@ -37,19 +43,19 @@ public static class JMath
      * inputs: a float to test with ("a")
      * outputs: if the number has a decimal or not
      */
-    public static bool HasDecimals(float a)
+    public static bool HasDecimals(this float a)
     {
         return a % 1 == 0;
     }
 
     // see above; overload
-    public static bool HasDecimals(double a)
+    public static bool HasDecimals(this double a)
     {
         return a % 1 == 0;
     }
 
     // see above; overload
-    public static bool HasDecimals(decimal a)
+    public static bool HasDecimals(this decimal a)
     {
         return a % 1 == 0;
     }
@@ -59,34 +65,14 @@ public static class JMath
      * inputs: a float to test with ("a")
      * outputs: if the number has a decimal or not
      */
-    public static int LargerTillInt(float a)
+    public static int LargerTillInt(this float a)
     {
         float b = a;
-        while (!HasDecimals(b)) // while b does not have a decimal
+        byte c = 0;
+        while (!HasDecimals(b) && c <= JMath.MAXINTLENGTH) // while b does not have a decimal
         {
             b *= 10; // multiply it by 10
-        }
-        return (int)b; // return it as an int
-    }
-
-    // see above; overload
-    public static int LargerTillInt(double a)
-    {
-        double b = a;
-        while (!HasDecimals(b)) // while b does not have a decimal
-        {
-            b *= 10; // multiply it by 10
-        }
-        return (int)b; // return it as an int
-    }
-
-    // see above; overload
-    public static int LargerTillInt(decimal a)
-    {
-        decimal b = a;
-        while (!HasDecimals(b)) // while b does not have a decimal
-        {
-            b *= 10; // multiply it by 10
+            ++c;
         }
         return (int)b; // return it as an int
     }
@@ -96,15 +82,60 @@ public static class JMath
      * inputs: a long to operate on ("a")
      * outputs: top half of a long to be an int
      */
-    public static int IntFromTopOfLong(long a)
+    public static int IntFromTopOfLong(this long a)
     {
         long b = a;
-        while (b >= JMath.MAXINT) // while b does not have a decimal
+        byte c = 0;
+        while (b >= JMath.MAXINT && c <= JMath.MAXINTLENGTH) // while b does not have a decimal
         {
             b /= 10; // multiply it by 10
+            ++c;
         }
         return (int)b; // return it as an int
     }
+
+    /*
+     * purpose: returns the lenght of a number
+     * inputs: an int to operate on ("a")
+     * outputs: the number of numbers in a number
+     */
+    public static int Length(this int a)
+    {
+        return (int)Math.Floor(Math.Log10(Math.Abs(a)) + 1);
+    }
+
+    // see above; overload
+    public static int Length(this long a)
+    {
+        return (int)Math.Floor(Math.Log10(Math.Abs(a)) + 1);
+    }
+
+    // see above; overload
+    public static int Length(this float a)
+    {
+        return (int)Math.Floor(Math.Log10(Math.Abs(a)) + 1);
+    }
+
+    // see above; overload
+    public static int Length(this double a)
+    {
+        return (int)Math.Floor(Math.Log10(Math.Abs(a)) + 1);
+    }
+
+    // see above; overload
+    public static int Length(this byte a)
+    {
+        return (int)Math.Floor(Math.Log10(Math.Abs(a)) + 1);
+    }
+
+    // see above; overload
+    public static int Length(this decimal a)
+    {
+        // convert to a non-negative, then to a string, get rid of decimal marks, and get the length
+        // this is because Math.Log10 does not take decimals, nor does unity's Mathf.Log10
+        return Math.Abs(a).ToString().Replace(".", string.Empty).Length;
+    }
+
 
     #endregion
 }
