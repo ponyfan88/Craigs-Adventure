c4ec378ca41e19b6299cfb5de5ace9edd87f6bdd
XanderMooney <PS24XMooney@efcts.us>
2022-11-22 19:15:11 -0500

    explosion changes

--- Assets/Scripts/Projectiles/collisionBehaviors/DamageColliders.cs
+++ Assets/Scripts/Projectiles/collisionBehaviors/DamageColliders.cs
@@ -1,20 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+/* Programmer: Xander Mooney
+ * Purpose: Apply damage to anything that collides with the corresponding object, at all times.
+ * Inputs: 
+ * Outputs:
+ */
 using UnityEngine;
 
 public class DamageColliders : MonoBehaviour
 {
     [Min(0)]public int DamageAmount = 1; // amount of damage it deals to non-player objects
     public bool DamagePlayer = true, DamageEnemies = true;
+    Collider2D[] allOverlappingColliders;
+
+    public void Awake()
+    {
+        if (TryGetComponent(out BoxCollider2D boxCollider))
+        {
+            Vector2 center = transform.position;
+            center += new Vector2(boxCollider.offset.x, boxCollider.offset.y);
+
+            allOverlappingColliders = Physics2D.OverlapBoxAll(center, boxCollider.size, 0);
+        }
+        else if (TryGetComponent(out CircleCollider2D circleCollider))
+        {
+            Vector2 center = transform.position;
+            center += new Vector2(circleCollider.offset.x, circleCollider.offset.y);
+
+            allOverlappingColliders = Physics2D.OverlapCircleAll(center, circleCollider.radius);
+        }
+
+        foreach (Collider2D collider in allOverlappingColliders)
+        {
+            OnTriggerEnter2D(collider);
+        } 
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log(collision.name + " Attempted Take Damage!");
         if (collision.TryGetComponent(out healthManager Enemyhealth))
         {
             if (DamageEnemies && collision.name != "player")
             {
                 Enemyhealth.TakeDamage(DamageAmount);
             }
-            else if (DamagePlayer && collision.name == "player")
+            if (DamagePlayer && collision.name == "player")
             {
                 Enemyhealth.TakeDamage(1);
             }
