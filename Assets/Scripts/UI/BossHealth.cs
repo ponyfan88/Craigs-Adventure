d2a24e920c8c5f818484b36c393b6af1e175c6de
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-11-28 09:52:19 -0500

    boss health bar

---
+++ Assets/Scripts/UI/BossHealth.cs
@@ -0,0 +1,82 @@
+/* 
+ * Programmers: Jack Kennedy
+ * Purpose: display boss health
+ * Inputs: boss health and if we are paused
+ * Outputs: a large health bar on screen
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    #region Variables
+
+    // health we store to update
+    private int storedHealth = -1;
+
+    // the bars themselves
+    [SerializeField] private GameObject bossBarFG;
+    [SerializeField] private GameObject bossBarBG;
+
+    // our boss health
+    private healthManager bossHealth;
+
+    // bool for if our boss has died
+    private bool bossDied = false;
+
+    #endregion
+
+    #region Default Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossHealth = GameObject.FindGameObjectWithTag("Boss").GetComponent<healthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Pause.paused)
+        {
+            if (!bossDied)
+            {
+                bossBarBG.SetActive(true);
+
+                // if we're on the boss floor and our boss isnt dead and we arent paused
+                if (FloorManager.floor == 4)
+                {
+                    // if the boss health has changed
+                    if (bossHealth.health != storedHealth)
+                    {
+                        // assuming the boss isnt dead
+                        if (bossHealth.health != 0)
+                        {
+                            // scale the bar according to health over maxhealth, meaning at half health the bar will be at x scale 0.5f
+                            bossBarFG.transform.localScale = new Vector3(((float)bossHealth.health / (float)bossHealth.maxHealth), 1f, 1f);
+                            // update our stored health
+                            storedHealth = bossHealth.health;
+                        }
+                        else // our boss IS dead
+                        {
+                            // set our boss to be dead
+                            bossDied = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                bossBarBG.SetActive(false);
+            }
+        }
+        else
+        {
+            bossBarBG.SetActive(false);
+        }
+    }
+
+    #endregion
+}
