d319e1bce7c3bb8a4f8192571a742286940e4506
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-11-29 11:05:44 -0500

    pointer script

---
+++ Assets/Scripts/UI/PointToEnd.cs
@@ -0,0 +1,40 @@
+/* 
+ * Programmers: Jack Kennedy
+ * Purpose: display boss health
+ * Inputs: boss health and if we are paused
+ * Outputs: a large health bar on screen
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointToEnd : MonoBehaviour
+{
+    #region Variables
+
+    private float direction = 0f;
+
+    private gameObject player;
+    private gameObject endRoom;
+
+    #endregion
+
+    #region Default Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player"); // grab our player
+        endRoom = GameObject.FindWithTag("End Room");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Mathf.Atan2(player.transform.position.y - endRoom.transform.position.y, player.transform.position.x - endRoom.transform.position.x) * Mathf.Rad2Deg;
+        transform.Rotate(0f, direction, 0f);
+    }
+
+    #endregion
+}
