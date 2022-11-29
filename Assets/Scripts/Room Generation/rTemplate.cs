54ebae5e07317b25de7804734d328dc28be559dd
unknown <PS24CKowalewski@efcts.us>
2022-11-29 10:41:37 -0500

    rooms generating way too much fix
    basically i fixed dupe rooms

--- Assets/Scripts/Room Generation/rTemplate.cs
+++ Assets/Scripts/Room Generation/rTemplate.cs
@@ -5,18 +5,26 @@
  * Outputs: Used by rGen and globalGen to pull prefabs for instantiating (cloning)
  */
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class rTemplate : MonoBehaviour
 {
-    public GameObject[] tRooms; //storage/reference for rooms with a top/north-facing wall
-    public GameObject[] bRooms; //storage/reference for rooms with a bottom/south-facing wall
-    public GameObject[] lRooms; //storage/reference for rooms with a left-facing wall
-    public GameObject[] rRooms; //storage/reference for rooms with a right-facing wall
+    [SerializeField] private GameObject[] topRooms; //default state of trooms
+    [SerializeField] private GameObject[] botRooms; //default state of brooms
+    [SerializeField] private GameObject[] lefRooms; //default state of lrooms
+    [SerializeField] private GameObject[] rigRooms; //default state of rrooms
 
-    public GameObject[] mutliRooms;
-    public GameObject[] testSpawns;
+    public List<GameObject> tRooms = new List<GameObject>(); //storage/reference for rooms with a top/north-facing wall
+    public List<GameObject> bRooms = new List<GameObject>(); //storage/reference for rooms with a bottom/south-facing wall
+    public List<GameObject> lRooms = new List<GameObject>(); //storage/reference for rooms with a left-facing wall
+    public List<GameObject> rRooms = new List<GameObject>(); //storage/reference for rooms with a right-facing wall
 
     public GameObject[] Walls; //storage/reference for extra walls (to block off doors that lead to unspawned rooms and such)
     public GameObject[] Boss; //end rooms (currently only staircase rooms)
+
+    public void resetTemplate()
+    {
+        //reset lists to default values (called between floors)
+    }
 }
