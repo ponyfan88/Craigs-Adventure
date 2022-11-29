f132402bc95298d432d521b3a93085e6c44230a4
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-11-29 12:10:44 -0500

    pointer in game

--- Assets/Scripts/UI/PointToEnd.cs
+++ Assets/Scripts/UI/PointToEnd.cs
@@ -11,10 +11,17 @@ public class PointToEnd : MonoBehaviour
 {
     #region Variables
 
+    public float offset = 0f;
+
     private float direction = 0f;
 
     private GameObject player;
     private GameObject endRoom;
+
+    private bool gotdist = false;
+    private float startingDistance;
+
+    SpriteRenderer pointer;
 
     #endregion
 
@@ -25,13 +32,32 @@ public class PointToEnd : MonoBehaviour
     {
         player = GameObject.Find("player"); // grab our player
         endRoom = GameObject.FindWithTag("End Room");
+        pointer = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction = Mathf.Atan2(player.transform.position.y - endRoom.transform.position.y, player.transform.position.x - endRoom.transform.position.x) * Mathf.Rad2Deg;
-        transform.Rotate(0f, direction, 0f);
+        if (endRoom != null) // if we've found the endroom
+        {
+            // only get the starting distance on the first frame we find the end room
+            if (!gotdist)
+            {
+                startingDistance = JMath.Distance(endRoom.transform, player.transform);
+                gotdist = true;
+            }
+
+            direction = Mathf.Atan2(endRoom.transform.position.y - player.transform.position.y, endRoom.transform.position.x - player.transform.position.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, direction);
+
+            // scale pointer alpha with our distance, the closer we are the less we should see the pointer
+            pointer.color = new Color(1f, 1f, 1f, JMath.Distance(endRoom.transform, player.transform) / startingDistance);
+        }
+        else // we cannot find the endroom
+        {
+            // try and find it for next frame
+            endRoom = GameObject.FindWithTag("End Room");
+        }
     }
 
     #endregion
