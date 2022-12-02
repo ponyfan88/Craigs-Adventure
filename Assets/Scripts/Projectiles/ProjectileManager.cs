e0d146dbe614b18e7078f81c440832dea3f3305e
AlmnoA <113357290+AlmnoA@users.noreply.github.com>
2022-12-02 00:14:50 -0500

    projectile should properly reused themselves read disc
    the issue was the name of the game object in bullet[] was name rock(Clone) but the name of the bullet resource was named rock

--- Assets/Scripts/Projectiles/ProjectileManager.cs
+++ Assets/Scripts/Projectiles/ProjectileManager.cs
@@ -35,7 +35,7 @@ public class ProjectileManager : MonoBehaviour
     {
         for (int i = 0; i < bullets.Count; ++i)
         {
-            if (!bullets[i].activeSelf&& bullets[i].name==name) // resues old objects instead of insatiating more objects
+            if (!bullets[i].activeSelf && bullets[i].name == name +"(Clone)") // resues old objects instead of insatiating more objects
             {
                 bullets[i].GetComponent<Projectile>().ResetTimer();//gets bullet and resets it's timer
                 bullets[i].SetActive(true); //set it active
