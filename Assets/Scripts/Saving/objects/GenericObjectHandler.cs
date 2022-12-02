24e2b5850ae34b9b7126cad37b2b5d4e28f28b9b
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-12-02 12:00:08 -0500

    make sure we dont crash lmao

--- Assets/Scripts/Saving/objects/GenericObjectHandler.cs
+++ Assets/Scripts/Saving/objects/GenericObjectHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomManager : MonoBehaviour
+public class GenericObjectHandler : MonoBehaviour
 {
     #region Variables
 
