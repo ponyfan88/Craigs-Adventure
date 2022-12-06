2941a61f292ad428b8c15406dbbe1cc46f8f6770
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-12-06 09:06:06 -0500

    remove unused property

--- Assets/Scripts/Saving/objects/GenericObject.cs
+++ Assets/Scripts/Saving/objects/GenericObject.cs
@@ -5,13 +5,11 @@
  * Outputs: (mirrors inputs)
  */
 
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GenericObject
 {
     // both equal null by default
-    public GameObject thingParent = null;
     public GameObject itemEnemyThing = null;
     public int parentUniqueID = 0;
 }
