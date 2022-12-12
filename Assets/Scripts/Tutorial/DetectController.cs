b87924aa10d6e20d4a3e379edd93b066555a27d2
unknown <PS24CKowalewski@efcts.us>
2022-12-12 10:37:45 -0500

    detect controller script set up

---
+++ Assets/Scripts/Tutorial/DetectController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectController : MonoBehaviour
+{
+    #region Variables
+
+    public bool latestKeyboard = true;
+
+    #endregion
+
+    #region Default Methods
+
+    private void Update()
+    {
+        if (false)
+        {
+
+        }
+    }
+
+    #endregion
+}
