eb1ff7d67cb721d39b56359a058cae685b268080
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2022-12-15 10:36:39 -0500

    correct floor display

--- Assets/Scripts/UI/TowerBase.cs
+++ Assets/Scripts/UI/TowerBase.cs
@@ -15,8 +15,6 @@ public class TowerBase : MonoBehaviour
     [SerializeField] private Sprite outsideTower; // tower when floor == 1
     [SerializeField] private Sprite tower; // tower when floor >= 2
 
-    private Sprite currentTowerSprite;
-
     #endregion
 
     #region Default Methods
@@ -24,16 +22,14 @@ public class TowerBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentTowerSprite = GetComponent<SpriteRenderer>().sprite;
-
         // we dont need to do this every frame since every new floor we restart the scene
         if (FloorManager.floor == 1)
         {
-            currentTowerSprite = outsideTower;
+            GetComponent<SpriteRenderer>().sprite = outsideTower;
         }
         else
         {
-            currentTowerSprite = tower;
+            GetComponent<SpriteRenderer>().sprite = tower;
         }
     }
 
