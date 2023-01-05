26f390d47af9288e4ca6fc12eac4ad0ff738be21
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-01-05 08:33:09 -0500

    dragon shoots mad fireballs

--- Assets/Scripts/AI/Enemy Specific/DragonStateManager.cs
+++ Assets/Scripts/AI/Enemy Specific/DragonStateManager.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
+[RequireComponent(typeof(ProjectileSpawner))]
 public class DragonStateManager : MonoBehaviour
 {
     #region Variables
 
     Transform player;
-    bool followPlayerX = true;
-    [SerializeField] private float speed = 5f;
+    ProjectileSpawner projectileSpawner;
+    public bool followPlayerX = true;
+    const float speed = 2f;
 
     #endregion
 
@@ -15,18 +17,26 @@ public class DragonStateManager : MonoBehaviour
     void Awake()
     {
         player = GameObject.Find("player").transform;
+        projectileSpawner = GetComponent<ProjectileSpawner>();
     }
 
     void Update()
     {
-        if (followPlayerX)
+        if (followPlayerX) // if the Ai currently asks for the dragon to follow the players X position
         {
+            if (player.position.y > 7) // cancel movement if the player is beside the dragon; prevents clipping out of map or getting stuck
+            {
+                return;
+            }
+            // calculate distance from player
             float dist = JMath.Distance(transform.position.x, player.position.x);
-            Debug.Log(dist + ", " + speed *Time.deltaTime);
+
+            // if the dragon can move without moving past the player then move towards the player
             if (dist > speed * Time.deltaTime)
             {
                 transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
             }
+            // check in negative direction
             else if (dist < -(speed * Time.deltaTime))
             {
                 transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
@@ -36,5 +46,15 @@ public class DragonStateManager : MonoBehaviour
     #endregion
 
     #region Custom Methods
+
+    /* Purpose: Allow the animator to call a function to spawn the dragons fireball attack
+     * Input: When to attack
+     * Output: Attack
+     */
+    void Fireball()
+    {
+        projectileSpawner.spawnerController(0);
+    }
+
     #endregion
 }
