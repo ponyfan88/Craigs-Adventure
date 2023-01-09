22284b6088970d4a9938ac47999689b3ad1f1c61
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2023-01-09 08:27:55 -0500

    DO YOU EVER FEEL

--- Assets/Scripts/UI/Credits/Name.cs
+++ Assets/Scripts/UI/Credits/Name.cs
@@ -20,21 +20,24 @@ public class Name : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     #endregion
 
     #region Default Methods
 
     void FixedUpdate()
     {
+        // move randomly every frame :)
+
         a += Time.deltaTime / 10;
 
         transform.position = new Vector3(transform.position.x + Random.Range(-a, a), transform.position.y + Random.Range(-a, a), transform.position.z + Random.Range(-a, a));
     }
 
+    // once we are offscreen
     private void OnBecameInvisible()
     {
         Debug.Log("i cannot be seen");
+        // set killme to true
         killme = true;
     }
 
