546d5dafb266000b1af70398e5ff4882ee3827bd
Playstation24Chris <PS24CKowalewski@efcts.us>
2023-01-11 10:14:54 -0500

    a few input changes...
    jk its just detecting whether controller or keyboard is used in the tutorial

--- Assets/Scripts/Tutorial/DetectController.cs
+++ Assets/Scripts/Tutorial/DetectController.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DetectController : MonoBehaviour
 {
     #region Variables
-
-    public bool latestKeyboard = true;
 
     #endregion
 
@@ -14,9 +13,15 @@ public class DetectController : MonoBehaviour
 
     private void Update()
     {
-        if (false)
+        if (Keyboard.current.anyKey.wasPressedThisFrame)
         {
-
+            gameObject.transform.Find("KeyboardPrompts").gameObject.SetActive(true);
+            gameObject.transform.Find("ControllerPrompts").gameObject.SetActive(false);
+        }
+        else if (Gamepad.current.buttonNorth.wasPressedThisFrame || Gamepad.current.buttonEast.wasPressedThisFrame || Gamepad.current.buttonSouth.wasPressedThisFrame || Gamepad.current.buttonWest.wasPressedThisFrame)
+        {
+            gameObject.transform.Find("ControllerPrompts").gameObject.SetActive(true);
+            gameObject.transform.Find("KeyboardPrompts").gameObject.SetActive(false);
         }
     }
 
