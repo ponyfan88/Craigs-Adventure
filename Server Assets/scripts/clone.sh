6704c68fcb0f2bffe7b561a7d566d8021d7510f0
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2023-04-20 10:52:38 -0400

    Update clone.sh

--- Server Assets/scripts/clone.sh
+++ Server Assets/scripts/clone.sh
@@ -1,11 +1,15 @@
-find ./html -mindepth 1 ! -regex '^./html/test\(/.*\)?' -delete
-cd html
-git clone https://github.com/ponyfan88/Craigs-Adventure.git
+# clone.sh by jack kennedy (ponyfan88)
+# purpose: removes the previous build of craigs adventure from the VPS and downloads the latest
+# feel free to use this in any project or whatever i dont care i just publish this to help anyone else out
 
-cd Craigs-Adventure
+find ./html -mindepth 1 ! -regex '^./html/test\(/.*\)?' -delete # delete every file in /var/www/html/ except for the "test" folder.
+cd html # cd into that folder
+git clone https://github.com/ponyfan88/Craigs-Adventure.git # clone craigs-adventure into /var/www/html/
 
-git checkout CraigGL
-git switch CraigGL
+cd Craigs-Adventure # cd into craigs adventure so we can use git commands
 
-cd ..
-mv Craigs-Adventure/* ../*
+git checkout CraigGL # grab the webgl branch
+git switch CraigGL # switch to said branch
+
+cd .. # cd out
+mv Craigs-Adventure/* ../* # move every file from /var/www/html/Craigs-Adventure/* to /var/www/html/* so that people actually can access it
